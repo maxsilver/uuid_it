1954a37255cac89f2a43e2da7abb2db0d6b317a3
Matthew Seeley <matthew@threadlight.com>
2013-01-17 15:25:03 -0500

    Moving uuid from separate object to the ActiveRecord object's table.

--- lib/uuid_it.rb
+++ lib/uuid_it.rb
@@ -6,41 +6,52 @@ require 'uuidtools'
 
 module ActiveRecord
   module Acts
-    
+
     module UuidIt
+      # def self.included(base)
+      #   @@classes ||= []
+      #   binding.pry
+      #   @@classes << self
+      # end
+
       def uuid_it
         class_eval do
           send :include, InstanceMethods
-          has_one :uuid_object, :as => :uuidable, :class_name => "Uuid", :dependent => :destroy
+          # has_one :uuid_object, :as => :uuidable, :class_name => "Uuid", :dependent => :destroy
           after_create :assign_uuid
         end
+        self.included(self)
       end
-      
-      def find_by_uuid uuid
-        return Uuid.find_by_uuidable_type_and_uuid(self.name, uuid).try(:uuidable)
-      end
+
+      # def uuid_classes
+      #   @@classes.uniq
+      # end
+
+      # def find_by_uuid uuid
+        # return Uuid.find_by_uuidable_type_and_uuid(self.name, uuid).try(:uuidable)
+      # end
 
       module InstanceMethods
         def uuid
-          assign_uuid unless self.uuid_object
-          self.uuid_object.uuid
+          assign_uuid unless self.read_attribute(:uuid).present?
+          self.read_attribute(:uuid)
         end
 
-        def uuid=(value)
-          if self.uuid_object.present?
-            self.uuid_object.update_attributes(:uuid => value)
-          else
-            self.build_uuid_object(:uuid => value)
-          end
-        end
+        # def uuid=(value)
+        #   if self.uuid.present?
+        #     self.uuid_object.update_attributes(:uuid => value)
+        #   else
+        #     self.build_uuid_object(:uuid => value)
+        #   end
+        # end
 
         def assign_uuid
-          if self.uuid_object.present?
-            self.uuid_object.save
+          if self.read_attribute(:uuid).present?
+            self.save
           else
             # self.build_uuid_object(:uuid => UUID.create.to_s)
             # Use external library for building UUIDs
-            self.build_uuid_object(:uuid => ::UUIDTools::UUID.random_create.to_s)
+            self.uuid = ::UUIDTools::UUID.random_create.to_s
             self.save unless self.new_record?
           end
         end
